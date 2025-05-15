19f1b5709713060ac7687db5cc0590ea4814b9f1
memo <alvaro@smartseals.co>
2025-05-15 10:18:33 -0500

    agregando las migraciones

--- internal/app/app.go
+++ internal/app/app.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/alvaromemo/femProject/internal/api"
 	"github.com/alvaromemo/femProject/internal/store"
+	"github.com/alvaromemo/femProject/migrations"
 )
 
 type Application struct {
@@ -19,11 +20,15 @@ type Application struct {
 
 func NewApplication() (*Application, error) {
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-
 	// our stores will go here
 	pgDb, err := store.Open()
 	if err != nil {
 		return nil, err
+	}
+
+	err = store.MigrateFS(pgDb, migrations.FS, ".")
+	if err != nil {
+		panic(err)
 	}
 
 	// our handlers will go here
